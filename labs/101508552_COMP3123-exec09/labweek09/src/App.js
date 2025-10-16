2b9659452b83468ce0c74c2cce25e5feee798b68
rishamsandhuu <rishamsandhuu@gmail.com>
2025-10-15 22:53:09 -0400

    Adding week 9

---
+++ labs/101508552_COMP3123-exec09/labweek09/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <p>Welcome to Fullstack Development - 1</p>
+        <p>React JS Programming Week09 Lab Exercise</p>
+        <p>101508552</p>
+        <p>Rishamnoor Kaur</p>
+        <p>George Brown College, Toronto</p>
+
+      </header> 
+    </div>
+  );
+}
+
+export default App;
